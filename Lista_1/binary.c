bda0cbb101bb624ca0fa959a7aea1c2a48be99c0
Lucas Brilhante <lbrilhante82@gmail.com>
2018-03-21 09:33:14 -0300

    Adicionando Busca binaria em C

---
+++ Lista_1/binary.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+ 
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+ 
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+ 
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+ 
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+int binary(int search,int array[], int n){
+    int c, first, last, middle;
+    first = 0;
+    last = n - 1;
+    middle = (first+last)/2;
+
+    while (first <= last) {
+        if (array[middle] < search)
+            first = middle + 1;    
+        else if (array[middle] == search) {
+            printf("%d found at location %d.\n", search, middle+1);
+            break;
+        }
+        else
+            last = middle - 1;
+
+        middle = (first + last)/2;
+    }
+    if (first > last)
+        printf("Not found! %d is not present in the list.\n", search);
+
+    return 0;   
+}
+
+
+int main(){
+    int n = 1000000;
+
+    int my_array[n];
+    srand(0);
+    int i;
+    
+    for (i = 0; i < n; i++) {
+        my_array[i] = rand()%n;
+        //printf("%d ",my_array[i]);
+    }
+    clock_t t;
+    t = clock();
+    quickSort(my_array,0,n);
+    binary(rand()%n,my_array,n);
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+    printf("It took %lf\n",time_taken);
+        
+}
